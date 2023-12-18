edc09c52b80079507b1a6b7282ffd75f5595f611
{takkun-mint} <{takuma.11.15@outlook.jp}>
2023-12-18 19:59:25 +0900

    Ver 4.99.10アップデート

--- Update_Check/Update.bat
+++ Update_Check/Update.bat
@@ -10,11 +10,13 @@ timeout 2 > NUL
 rmdir /s /q bin
 rmdir /s /q en-US
 rmdir /s /q ja-JP
+rmdir /s /q ru-RU
 
 move /y .\update\* .\
 move /y .\update\bin .\
 move /y .\update\en-US .\
 move /y .\update\ja-JP .\
+move /y .\update\ru-RU .\
 rmdir /s /q update
 del /q Update.zip
 del /q update.txt
